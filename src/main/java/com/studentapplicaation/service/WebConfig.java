24157a8756056cf58d5830e32fcec66f02513523
RakeshRocky220 <kandhalarakeshreddy220.com>
2023-04-26 00:19:48 +0530

    git link canges

--- src/main/java/com/studentapplicaation/service/WebConfig.java
+++ src/main/java/com/studentapplicaation/service/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
    @Override
    public void addCorsMappings(CorsRegistry registry) {
        registry.addMapping("/**")
-               .allowedOrigins("https://RakeshRocky220.github.io/")
+               .allowedOrigins("https://rakeshrocky220.github.io/")
                .allowedMethods("GET", "POST", "PUT", "DELETE")
                .allowedHeaders("*");
    }
